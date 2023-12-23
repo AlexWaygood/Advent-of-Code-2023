dbe6c99802a066ad11f16fef85c3fe4bfca17769
AlexWaygood <alex.waygood@gmail.com>
2023-12-23 15:57:31 +0000

    day 14b solution

--- day-14b/src/main.rs
+++ day-14b/src/main.rs
@@ -82,12 +82,12 @@ impl Platform {
                     let other_tile = self.tile_map.get(&other_coord).unwrap();
                     match other_tile {
                         &Tile::CubeRock => {
-                            y = following_y + 1;
                             if following_y == (self.max_y - 1) {
                                 break 'outer_column_loop;
                             };
+                            y = following_y + 1;
                             continue 'outer_column_loop;
-                        },
+                        }
                         &Tile::RoundRock => {
                             self.tile_map.insert(coord, Tile::RoundRock);
                             self.tile_map.insert(other_coord, Tile::Empty);
@@ -95,37 +95,182 @@ impl Platform {
                                 break 'outer_column_loop;
                             };
                             break;
-                        },
+                        }
                         &Tile::Empty => {
                             if following_y == (self.max_y - 1) {
                                 break 'outer_column_loop;
-                            };                           
+                            };
+                            continue;
                         }
                     }
                 }
                 y += 1;
-            };
+            }
         }
+    }
+
+    fn tilt_south(&mut self) {
+        for x in (0..self.max_x).rev() {
+            let mut y = self.max_y - 1;
+            'outer_column_loop: loop {
+                if y == 0 {
+                    break;
+                }
+                let coord = Coordinate(x, y);
+                let this_tile = self.tile_map.get(&coord).unwrap();
+                if this_tile != &Tile::Empty {
+                    y -= 1;
+                    continue;
+                }
+                for following_y in (0..y).rev() {
+                    let other_coord = Coordinate(x, following_y);
+                    let other_tile = self.tile_map.get(&other_coord).unwrap();
+                    match other_tile {
+                        &Tile::CubeRock => {
+                            if following_y == 0 {
+                                break 'outer_column_loop;
+                            };
+                            y = following_y - 1;
+                            continue 'outer_column_loop;
+                        }
+                        &Tile::RoundRock => {
+                            self.tile_map.insert(coord, Tile::RoundRock);
+                            self.tile_map.insert(other_coord, Tile::Empty);
+                            if following_y == 0 {
+                                break 'outer_column_loop;
+                            };
+                            break;
+                        }
+                        &Tile::Empty => {
+                            if following_y == 0 {
+                                break 'outer_column_loop;
+                            };
+                            continue;
+                        }
+                    }
+                }
+                y -= 1;
+            }
+        }
+    }
+
+    fn tilt_west(&mut self) {
+        for y in 0..self.max_y {
+            let mut x = 0;
+            'outer_column_loop: loop {
+                if x == (self.max_x - 1) {
+                    break;
+                }
+                let coord = Coordinate(x, y);
+                let this_tile = self.tile_map.get(&coord).unwrap();
+                if this_tile != &Tile::Empty {
+                    x += 1;
+                    continue;
+                }
+                for following_x in (x + 1)..self.max_x {
+                    let other_coord = Coordinate(following_x, y);
+                    let other_tile = self.tile_map.get(&other_coord).unwrap();
+                    match other_tile {
+                        &Tile::CubeRock => {
+                            if following_x == (self.max_x - 1) {
+                                break 'outer_column_loop;
+                            };
+                            x = following_x + 1;
+                            continue 'outer_column_loop;
+                        }
+                        &Tile::RoundRock => {
+                            self.tile_map.insert(coord, Tile::RoundRock);
+                            self.tile_map.insert(other_coord, Tile::Empty);
+                            if following_x == (self.max_x - 1) {
+                                break 'outer_column_loop;
+                            };
+                            break;
+                        }
+                        &Tile::Empty => {
+                            if following_x == (self.max_x - 1) {
+                                break 'outer_column_loop;
+                            };
+                            continue;
+                        }
+                    }
+                }
+                x += 1;
+            }
+        }
+    }
+
+    fn tilt_east(&mut self) {
+        for y in 0..self.max_y {
+            let mut x = self.max_x - 1;
+            'outer_column_loop: loop {
+                if x == 0 {
+                    break;
+                }
+                let coord = Coordinate(x, y);
+                let this_tile = self.tile_map.get(&coord).unwrap();
+                if this_tile != &Tile::Empty {
+                    x -= 1;
+                    continue;
+                }
+                for following_x in (0..x).rev() {
+                    let other_coord = Coordinate(following_x, y);
+                    let other_tile = self.tile_map.get(&other_coord).unwrap();
+                    match other_tile {
+                        &Tile::CubeRock => {
+                            if following_x == 0 {
+                                break 'outer_column_loop;
+                            };
+                            x = following_x - 1;
+                            continue 'outer_column_loop;
+                        }
+                        &Tile::RoundRock => {
+                            self.tile_map.insert(coord, Tile::RoundRock);
+                            self.tile_map.insert(other_coord, Tile::Empty);
+                            if following_x == 0 {
+                                break 'outer_column_loop;
+                            };
+                            break;
+                        }
+                        &Tile::Empty => {
+                            if following_x == 0 {
+                                break 'outer_column_loop;
+                            };
+                            continue;
+                        }
+                    }
+                }
+                x -= 1;
+            }
+        }
+    }
+
+    fn cycle(&mut self) {
+        self.tilt_north();
+        self.tilt_west();
+        self.tilt_south();
+        self.tilt_east();
     }
 
     fn calculate_load(&self) -> u32 {
         let mut answer = 0;
-        let y_to_load_map = HashMap::<u32, u32>::from_iter(
-            (1..(self.max_y + 1))
-                .rev()
-                .enumerate()
-                .map(|(i, y)| (i.try_into().unwrap(), y)),
-        );
+        let y_to_load_map = Vec::from_iter((1..(self.max_y + 1)).rev());
         for x in 0..self.max_x {
             for y in 0..self.max_y {
                 let coord = Coordinate(x, y);
                 if self.tile_map.get(&coord).unwrap() == &Tile::RoundRock {
-                    answer += y_to_load_map.get(&y).unwrap();
+                    answer += y_to_load_map[y as usize];
                 }
             }
         }
         answer
     }
+
+    // Convenience method for easier testing:
+    #[cfg(test)]
+    fn to_string(&self) -> String {
+        String::from(format!("{}", self).trim())
+    }
+
 }
 
 impl FromStr for Platform {
@@ -173,9 +318,35 @@ fn parse_input(filename: &str) -> Result<Platform> {
         .parse()?)
 }
 
+// Given to us in the puzzle description
+const NUM_ITERATIONS_REQUIRED: usize = 1000000000;
+
+// Hardcoded number determined by observing the printed output of each iteration,
+// and realising that the values were cycling every 18 iterations
+const CYCLE_LENGTH: usize = 18;
+
 fn solve(filename: &str) -> u32 {
     let mut platform = parse_input(filename).unwrap();
-    platform.tilt_north();
+    let mut previous_record = [0; CYCLE_LENGTH];
+    let mut this_record = [1; CYCLE_LENGTH];
+    let mut i = 0;
+    loop {
+        let cycle_step = i % CYCLE_LENGTH;
+        if cycle_step == 0 {
+            if this_record == previous_record {
+                break
+            }
+            (previous_record, this_record) = (this_record, previous_record)
+        }
+        platform.cycle();
+        let load = platform.calculate_load();
+        this_record[cycle_step] = load;
+        i += 1
+    }
+    let jumps = (NUM_ITERATIONS_REQUIRED - i) % CYCLE_LENGTH;
+    for _ in 0..jumps {
+        platform.cycle();
+    }
     platform.calculate_load()
 }
 
@@ -191,9 +362,15 @@ mod tests {
         fs::read_to_string,
     };
 
+    const FILENAME: &str = "input.txt";
+
+    fn create_platform() -> Platform {
+        parse_input(FILENAME).unwrap()
+    }
+
     #[test]
     fn test_parsing_basics() {
-        let platform = parse_input("input.txt").unwrap();
+        let platform = create_platform();
         assert_eq!(platform.tile_map.len(), 10_000);
         assert_eq!(platform.max_x, 100);
         assert_eq!(platform.max_y, 100);
@@ -215,13 +392,12 @@ mod tests {
                 .trim(),
         );
         let platform: Platform = input.parse().unwrap();
-        let platform_display = String::from(format!("{}", platform).trim());
-        assert_eq!(platform_display, input)
+        assert_eq!(platform.to_string(), input)
     }
 
     #[test]
-    fn test_tilting() {
-        let mut platform = parse_input("input.txt").unwrap();
+    fn test_tilting_basics() {
+        let mut platform = create_platform();
         let tiles: HashMap<Coordinate, Tile> = platform
             .tile_map
             .iter()
@@ -244,6 +420,45 @@ mod tests {
     }
 
     #[test]
+    fn test_tilting_roundtrip() {
+        let mut platform = create_platform();
+
+        platform.tilt_north();
+        let platform_display_1 = platform.to_string();
+        platform.tilt_south();
+        let platform_display_2 = platform.to_string();
+        assert_ne!(platform_display_1, platform_display_2);
+        platform.tilt_north();
+        let platform_display_3 = platform.to_string();
+        assert_eq!(platform_display_1, platform_display_3);
+        platform.tilt_south();
+        let platform_display_4 = platform.to_string();
+        assert_eq!(platform_display_2, platform_display_4);
+
+        platform.tilt_east();
+        let platform_display_5 = platform.to_string();
+        platform.tilt_west();
+        let platform_display_6 = platform.to_string();
+        assert_ne!(platform_display_5, platform_display_6);
+        platform.tilt_east();
+        let platform_display_7 = platform.to_string();
+        assert_eq!(platform_display_5, platform_display_7);
+        platform.tilt_west();
+        let platform_display_8 = platform.to_string();
+        assert_eq!(platform_display_6, platform_display_8);
+    }
+
+    #[test]
+    fn test_cycle_basics() {
+        let mut platform = create_platform();
+        platform.cycle();
+        let platform_display = platform.to_string();
+        platform.tilt_east();
+        let platform_display_2 = platform.to_string();
+        assert_eq!(platform_display, platform_display_2)
+    }
+
+    #[test]
     fn test_coordinate() {
         let coord = Coordinate(0, 0);
         let coord2 = Coordinate(0, 0);
@@ -260,7 +475,7 @@ mod tests {
     }
 
     #[test]
-    fn test_examples() {
+    fn test_tilting_examples() {
         let input = "\
 O....#....
 O.OO#....#
@@ -273,7 +488,7 @@ O.#..O.#.#
 #....###..
 #OO..#....";
         let mut platform: Platform = input.parse().unwrap();
-        let platform_display = String::from(format!("{}", platform).trim());
+        let platform_display = platform.to_string();
         assert_eq!(input, platform_display.as_str());
 
         let tilted_input = "\
@@ -288,7 +503,7 @@ O..#.OO...
 #....###..
 #....#....";
         platform.tilt_north();
-        let new_platform_display = String::from(format!("{}", platform).trim());
+        let new_platform_display = platform.to_string();
         assert_eq!(
             tilted_input,
             new_platform_display.as_str(),
@@ -296,5 +511,68 @@ O..#.OO...
             new_platform_display
         );
         assert_eq!(platform.calculate_load(), 136)
+    }
+
+    #[test]
+    fn test_cycle_examples() {
+        let input = "\
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+        let mut platform: Platform = input.parse().unwrap();
+        let platform_display = platform.to_string();
+        assert_eq!(input, platform_display.as_str());
+
+        let cycled_input = "\
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....";
+        platform.cycle();
+        let cycled_platform_display = platform.to_string();
+        assert_eq!(cycled_input, cycled_platform_display.as_str());
+
+        let cycled_input_2 = "\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O";
+        platform.cycle();
+        let cycled_platform_display_2 = platform.to_string();
+        assert_eq!(cycled_input_2, cycled_platform_display_2.as_str());
+
+        let cycled_input_3 = "\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O";
+        platform.cycle();
+        let cycled_platform_display_3 = platform.to_string();
+        assert_eq!(cycled_input_3, cycled_platform_display_3.as_str());
     }
 }
