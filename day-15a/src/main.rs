a9a6dd29fb0d1fdab23a395b171751abd9850864
AlexWaygood <alex.waygood@gmail.com>
2023-12-27 10:48:42 +0000

    day-15a solution

---
+++ day-15a/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::read_to_string;
+
+use cached::proc_macro::cached;
+
+#[cached]
+fn run_algorithm(step: String) -> u8 {
+    debug_assert!(step.is_ascii());
+    let mut answer: u32 = 0;
+    for byte in step.bytes() {
+        answer += byte as u32;
+        answer *= 17;
+        answer %= 256
+    }
+    answer.try_into().expect("Expected result to be <256!")
+}
+
+fn read_input(filename: &str) -> String {
+    read_to_string(filename).expect(format!("Expected {} to exist!", filename).as_str())
+}
+
+fn solve(filename: &str) -> u32 {
+    read_input(filename)
+        .split(",")
+        .map(|step| (run_algorithm(step.to_string()) as u32))
+        .sum()
+}
+
+fn main() {
+    println!("{}", solve("input.txt"));
+}
