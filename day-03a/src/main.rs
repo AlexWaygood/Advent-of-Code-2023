49d57f7f44b77fd6e44c1e0a46a282ca2459a088
AlexWaygood <alex.waygood@gmail.com>
2024-01-27 10:20:16 +0000

    Complete day-03a. Fixes #1

--- day-03a/src/main.rs
+++ day-03a/src/main.rs
@@ -1,82 +1,72 @@
-use regex::Regex;
-use std::cmp::{max, min};
+use std::cmp::min;
+use std::collections::HashSet;
 use std::fs::read_to_string;
-use std::iter::Extend;
-use std::ops::Not;
-
-struct LocRange {
-    start: usize,
-    end: usize,
-}
+use std::ops::Range;
 
 fn gather_surrounding_chars(
-    loc_range: LocRange,
+    loc_range: Range<usize>,
     lineno: usize,
     line: &str,
-    all_lines: &Vec<&str>,
-) -> Vec<char> {
-    let mut answer = Vec::new();
-    let left = max(0, loc_range.start.wrapping_sub(1));
-    let right = min(all_lines[0].len(), loc_range.end);
-    match all_lines.get(lineno.wrapping_sub(1)) {
-        Some(prev_line) => answer.extend(prev_line[left..right].chars()),
-        None => {}
+    all_lines: &[&str],
+) -> HashSet<char> {
+    let left = loc_range.start.saturating_sub(1);
+    let right = min(all_lines[0].len() - 1, loc_range.end);
+    let mut answer = HashSet::new();
+    if let Some(prev_line) = all_lines.get(lineno.saturating_sub(1)) {
+        answer.extend(prev_line[left..=right].chars());
     }
-    match all_lines.get(lineno.wrapping_add(1)) {
-        Some(next_line) => answer.extend(next_line[left..right].chars()),
-        None => {}
+    if let Some(next_line) = all_lines.get(lineno + 1) {
+        answer.extend(next_line[left..=right].chars());
     }
     let line_as_bytes = line.as_bytes();
-    for c in [line_as_bytes[left], line_as_bytes[right.wrapping_sub(1)]] {
-        if c.is_ascii_digit().not() {
-            answer.push(c as char)
-        }
-    }
+    answer.insert(line_as_bytes[left] as char);
+    answer.insert(line_as_bytes[right] as char);
     answer
 }
 
 fn char_is_symbol(c: &char) -> bool {
-    let period: &char = &'.';
-    c.is_digit(10) && c != period
+    c != &'.' && !c.is_ascii_digit()
 }
 
-fn is_part_number(loc_range: LocRange, lineno: usize, line: &str, all_lines: &Vec<&str>) -> bool {
-    let surrounding_chars = gather_surrounding_chars(loc_range, lineno, line, all_lines);
-    surrounding_chars.iter().any(char_is_symbol)
+fn is_part_number(loc_range: Range<usize>, lineno: usize, line: &str, all_lines: &[&str]) -> bool {
+    gather_surrounding_chars(loc_range, lineno, line, all_lines)
+        .iter()
+        .any(char_is_symbol)
 }
 
-fn gather_part_numbers_from_line(lineno: usize, line: &str, all_lines: &Vec<&str>) -> Vec<u32> {
-    let mut answer = Vec::new();
-    let number_re = Regex::new(r"\d+").unwrap();
-    for number_match in number_re.find_iter(line) {
-        let loc_range = LocRange {
-            start: number_match.start(),
-            end: number_match.end(),
-        };
-        if is_part_number(loc_range, lineno, line, all_lines) {
-            let parsed_number = number_match.as_str().parse::<u32>().unwrap();
-            answer.push(parsed_number)
-        }
-    }
-    answer
+fn gather_part_numbers_from_line(lineno: usize, line: &str, all_lines: &[&str]) -> Vec<u32> {
+    let number_re = regex::Regex::new(r"\d+").expect("Thought this would be a valid regex");
+    number_re
+        .find_iter(line)
+        .filter(|needle| is_part_number(needle.range(), lineno, line, all_lines))
+        .map(|needle| {
+            needle
+                .as_str()
+                .parse()
+                .expect("Expected this to parse as a number")
+        })
+        .collect()
 }
 
-fn gather_part_numbers_from_file(lines: Vec<&str>) -> Vec<u32> {
-    let mut answer = Vec::new();
-    for (lineno, line) in lines.iter().enumerate() {
-        let found_parts = gather_part_numbers_from_line(lineno, line, &lines);
-        answer.extend(found_parts);
-    }
-    answer
+fn gather_part_numbers_from_file(input: String) -> Vec<u32> {
+    let lines: Vec<&str> = input.lines().collect();
+    lines
+        .iter()
+        .enumerate()
+        .flat_map(|(lineno, line)| gather_part_numbers_from_line(lineno, line, &lines))
+        .collect()
+}
+
+fn read_input(filename: &str) -> String {
+    read_to_string(filename)
+        .unwrap_or_else(|_| panic!("{}", format!("Expected {filename} to exist")))
 }
 
 fn solve(filename: &str) -> u32 {
-    let file = read_to_string(filename).unwrap();
-    let lines: Vec<&str> = file.lines().collect();
-    gather_part_numbers_from_file(lines).iter().sum()
+    let input = read_input(filename);
+    gather_part_numbers_from_file(input).iter().sum()
 }
 
 fn main() {
-    let answer = solve("src/input.txt");
-    println!("{}", answer)
+    println!("{}", solve("input.txt"));
 }
